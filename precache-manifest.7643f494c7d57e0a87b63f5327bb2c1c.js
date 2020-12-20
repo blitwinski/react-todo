b9b69cc37b60ded0af9f6befd98c8b6a477f0044
blitwinski <you@example.com>
2020-12-20 21:11:58 +0100

    Updates

--- precache-manifest.7643f494c7d57e0a87b63f5327bb2c1c.js
+++ precache-manifest.7643f494c7d57e0a87b63f5327bb2c1c.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dc918852f0a02e27e2dff4ea1949712c",
+    "revision": "d2e77a05694f356c293e39fa7a0ae35a",
     "url": "/index.html"
   },
   {
-    "revision": "6a1543e614972d59ebb4",
+    "revision": "ef9c61e673fb8ccde7a6",
     "url": "/static/css/main.79345fa9.chunk.css"
   },
   {
-    "revision": "111fc42f6f9f8cd15abb",
-    "url": "/static/js/2.0e5a29cf.chunk.js"
+    "revision": "016eb83f2f722521ddc1",
+    "url": "/static/js/2.699e39ce.chunk.js"
   },
   {
     "revision": "f5fc2afae58c4ffc30c6148864765d4b",
-    "url": "/static/js/2.0e5a29cf.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.699e39ce.chunk.js.LICENSE.txt"
   },
   {
     "revision": "a014af07e06a387c5dd9",
     "url": "/static/js/3.11d156c5.chunk.js"
   },
   {
-    "revision": "6a1543e614972d59ebb4",
-    "url": "/static/js/main.fad888d8.chunk.js"
+    "revision": "ef9c61e673fb8ccde7a6",
+    "url": "/static/js/main.2c171159.chunk.js"
   },
   {
     "revision": "7ea3f6ba95110382ac95",
