1dc443025bb108ca86470f9b380efd28232fca3c
Bogusław Litwiński <42648786+blitwinski@users.noreply.github.com>
2020-12-21 19:44:02 +0100

    Update precache-manifest.7643f494c7d57e0a87b63f5327bb2c1c.js

--- precache-manifest.7643f494c7d57e0a87b63f5327bb2c1c.js
+++ precache-manifest.7643f494c7d57e0a87b63f5327bb2c1c.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "d2e77a05694f356c293e39fa7a0ae35a",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "ef9c61e673fb8ccde7a6",
-    "url": "/static/css/main.79345fa9.chunk.css"
+    "url": "./static/css/main.79345fa9.chunk.css"
   },
   {
     "revision": "016eb83f2f722521ddc1",
-    "url": "/static/js/2.699e39ce.chunk.js"
+    "url": "./static/js/2.699e39ce.chunk.js"
   },
   {
     "revision": "f5fc2afae58c4ffc30c6148864765d4b",
-    "url": "/static/js/2.699e39ce.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.699e39ce.chunk.js.LICENSE.txt"
   },
   {
     "revision": "a014af07e06a387c5dd9",
-    "url": "/static/js/3.11d156c5.chunk.js"
+    "url": "./static/js/3.11d156c5.chunk.js"
   },
   {
     "revision": "ef9c61e673fb8ccde7a6",
-    "url": "/static/js/main.2c171159.chunk.js"
+    "url": "./static/js/main.2c171159.chunk.js"
   },
   {
     "revision": "7ea3f6ba95110382ac95",
-    "url": "/static/js/runtime-main.ccd0ebcb.js"
+    "url": "./static/js/runtime-main.ccd0ebcb.js"
   },
   {
     "revision": "06e733283fa43d1dd57738cfc409adbd",
-    "url": "/static/media/logo.06e73328.svg"
+    "url": "./static/media/logo.06e73328.svg"
   }
 ]);
